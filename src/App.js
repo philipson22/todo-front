16c4aa74115bbed04adf6efcf59fe17b02e50626
Philippe MOUIEL <philippemouiel@mbp-de-philippe.lan>
2018-11-26 11:44:05 +0100

    front en dur

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+import Item from "./components/Item";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="container">
+        <h1>TO-DO LIST</h1>
+        <p>
+          <button type="onClick">X</button>
+          <Item />
+        </p>
+        <p>
+          <button type="onClick">X</button>
+          <Item />
+        </p>
+        <p>
+          <button type="onClick">X</button>
+          <Item />
+        </p>
+        <p>
+          <button type="onClick">X</button>
+          <Item />
+        </p>
+
+        <form>
+          <input type="text" placeholder="Titre" />
+
+          <button
+            style={{
+              marginTop: "20px",
+              width: "250px",
+              height: "45px",
+              backgroundColor: "#814ED1",
+              borderRadius: "5px"
+            }}
+            type="submit"
           >
-            Learn React
-          </a>
-        </header>
+            AJOUTER UNE TACHE
+          </button>
+        </form>
       </div>
     );
   }
---
+++ src/components/Item.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Item extends React.Component {
+  render() {
+    return <fragment> item created</fragment>;
+  }
+}
+
+export default Item;
